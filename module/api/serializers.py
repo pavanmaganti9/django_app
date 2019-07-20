c13172053a09960d9cb3594bc8cdf2352e5bd655
PVPavanMaganti <pavan@markitome.com>
2019-07-20 13:22:43 +0530

    DRF searcH, auth and mixins

--- module/api/serializers.py
+++ module/api/serializers.py
@@ -3,6 +3,15 @@ from .models import Songs
 
 
 class SongsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Songs
-        fields = ('id',"title", "artist")
+	class Meta:
+		model = Songs
+		fields = ('id',"title", "artist")
+	
+	
+	def validate_title(self, value):
+		qs = Songs.objects.filter(title__iexact=value)
+		if self.instance:
+			qs = qs.exclude(id=self.instance.id)
+		if qs.exists():
+			raise serializers.ValidationError("Title must be unique")
+		return value
--- module/api/views.py
+++ module/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from django.db.models import Q
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, mixins
 from .models import Songs
 from .serializers import SongsSerializer
 
@@ -11,9 +11,19 @@ class ListSongsView(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Songs.objects.all()
 	serializer_class = SongsSerializer
 	
-class ListSongsView2(generics.ListAPIView):
+class ListSongsView2(mixins.CreateModelMixin, generics.ListAPIView):
 	queryset = Songs.objects.all()
 	serializer_class = SongsSerializer
+	
+	def get_queryset(self):
+		queryset = Songs.objects.all()
+		query = self.request.GET.get("q")
+		if query is not None:
+			queryset = queryset.filter(Q(title__icontains=query)|Q(artist__icontains=query)).distinct()
+			return queryset
+	
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, **kwargs)
 	
 class ListSongsCreateView(generics.CreateAPIView):
 	lookup_field = 'id'
